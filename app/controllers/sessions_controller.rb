bc38e5b59bce3cdbca0daf4a3a457146c8a20e0f
Attila Kiss <darkzbane@gmail.com>
2021-05-18 11:20:58 +0200

    Folder fix done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,9 @@
 class SessionsController < ApplicationController
   before_action :logged_in_redirect, only: [:new, :create]
+
   def new
   end
+
   def create
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
